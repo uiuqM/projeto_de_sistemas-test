6dd29d916c400162f6ee41693e26789393379d40
MateusAlvez <mateusrx1203@gmail.com>
2023-09-10 10:05:49 -0300

    Label_sair_do_game_mais_script

--- scripts/startScreen.gd
+++ scripts/startScreen.gd
@@ -1,13 +1,18 @@
 extends Control
 
+
 func _ready():
+	$controls/exit_btn/Label.hide()
 	$controls/start_btn.grab_focus()
 
 func _on_start_btn_pressed():
 	get_tree().change_scene("res://levels/gameScreen.tscn")
 
 func _on_exit_btn_pressed():
+	$controls/exit_btn/Label.show()
+	yield (get_tree().create_timer(1.0), "timeout")
 	get_tree().quit()
+	
 
 func _on_control_btn_pressed():
 	var  CrontrolScreen = load("res://levels/controlsScreen.tscn").instance()
