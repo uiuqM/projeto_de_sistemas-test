8c516afbe29c220a39805a97885584d87c868a36
MateusAlvez <mateusrx1203@gmail.com>
2023-09-16 12:22:46 -0300

    criando_dialogo_inimigo

---
+++ scripts/dialago.gd
@@ -0,0 +1,42 @@
+extends Control
+class_name dialago
+
+signal dialogo_finished
+
+onready var text : RichTextLabel = get_node("background/Text")
+
+var can_intarect : bool = false
+
+var current_dialogo: int = 0
+var dialogo_text_list : Array = []
+
+
+func _ready() -> void:
+	show_dialogo()
+
+func _precces (_delta: float) -> void:
+	if can_intarect and Input.is_action_just_pressed("ui_interact"):
+		can_intarect = false
+		current_dialogo += 1 
+		show_dialogo()
+
+func show_dialogo() -> void:
+	if current_dialogo == dialogo_text_list.size() - 1:
+		emit_signal("dialogo_finished")
+		queue_free()
+		return
+		
+	text.percent_visible = 0
+	text.text = dialogo_text_list[current_dialogo]
+	
+	
+	while text.percent_visible < 1:
+		text.percent_visible += get_process_delta_time()
+		yield (get_tree(), "idle_frame")
+		
+	can_intarect = true
+	
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://levels/levels_first_game/telaBatalhaScreen.tscn")
---
+++ scripts/xingling.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+class_name enemy
+
+
+var can_intarect: bool = false
+
+export (Array, String) var dialogo_text
+
+func _physics_process(_delta)->void:
+	if Input.is_action_just_pressed("ui_interact") and can_intarect:
+	 can_intarect = false
+	 print ("entrou em contato")
+	 get_tree().call_group("interface","spaw_dialogo",dialogo_text)
+
+
+func on_body_entered(body)->void:
+	if body.name == "player":
+		can_intarect =true
+		print("Personagem entrou em contato")
+
+func _on_Area2D_body_exited(body)->void:
+	if body.name == "player":
+		can_intarect= false
+		print("Personagem saiu do contato")
+		
+		
