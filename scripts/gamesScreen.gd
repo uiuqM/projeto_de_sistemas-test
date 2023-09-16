906ecefd9729e9f3524d2cba73c85da21bf46f1a
uiuqM <wilque.coelho@mail.uft.edu.br>
2023-09-16 19:56:39 -0300

    feature: adding-transition

--- scripts/gamesScreen.gd
+++ scripts/gamesScreen.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 func _ready():
-  pass
+	$controls/exit_btn/Label.hide()
+	$controls/game_one_btn.grab_focus()
 
 func _on_return_btn_pressed():
-	var _change_scene: bool = get_tree().change_scene("res://levels/startScreen.tscn")
+	SceneTransition.change_scene("res://levels/startScreen.tscn")
 	
 func _on_exit_btn_pressed():
 	get_tree().quit()
 
 func _on_game_one_btn_pressed():
-	get_tree().change_scene("res://levels/firstGameScreen.tscn")
+	SceneTransition.change_scene("res://levels/firstGameScreen.tscn")
