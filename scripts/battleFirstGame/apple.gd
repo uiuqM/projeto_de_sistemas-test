c8deb10ff630cc5d959b7c0b88a683015cbc17c5
Érick Santos <ericksantos@mailfence.com>
2023-11-04 18:04:56 -0300

    feat: adiciona HUD

--- scripts/battleFirstGame/apple.gd
+++ scripts/battleFirstGame/apple.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 signal geted
+export var fruits = 1 
 
 func _on_itens_body_entered(body):
 	print(body.name)
 	$anim.play("collected")
+	Global.fruits += fruits
+	print(Global.fruits)
 
 func _on_anim_animation_finished(anim_name):
 	if anim_name == "collected":
