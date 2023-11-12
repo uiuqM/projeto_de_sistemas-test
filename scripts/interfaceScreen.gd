462091f9d747b0cfa90cb7556ec0fee8d99e3c06
MateusAlvez <mateusrx1203@gmail.com>
2023-11-12 12:20:35 -0300

    chefe_segunda_fase

--- scripts/interfaceScreen.gd
+++ scripts/interfaceScreen.gd
@@ -1,11 +1,11 @@
 extends CanvasLayer
 class_name interface
 
-const DIALOGO: PackedScene = preload("res://scenes/start_screen_scenes/first_phase_screen_scenes/dialago.tscn")
+const DIALOGO: PackedScene = preload("res://scenes/start_screen_scenes/second_phase_screen_scenes/battle-2/dialago-1.tscn")
 
 
 
 func spaw_dialogo(dialogo_text_list:Array) -> void:
-		var dialogo: dialago = DIALOGO.instance()
+		var dialogo: dialago_1 = DIALOGO.instance()
 		dialogo.dialogo_text_list = dialogo_text_list
 		add_child(dialogo)
