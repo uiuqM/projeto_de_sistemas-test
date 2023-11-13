bbaeed745de6fd5b50e7a8d83ec5ba88a8b025f9
MateusAlvez <mateusrx1203@gmail.com>
2023-11-13 10:44:38 -0300

    chefes_arrumados_transição_de_fase

--- scripts/Reset_Level.gd
+++ scripts/Reset_Level.gd
@@ -1,7 +1,7 @@
 extends Node
 
-#const SAVE_GAME_PATH := "user://save.tres"
-#var checkpoint_pos = 0
+const SAVE_GAME_PATH := "user://save.tres"
+var checkpoint_pos = 0
 
 func _ready():
 	Global.fruits = 0
